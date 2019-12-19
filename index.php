e96b63be23a314f6ce0ed4345c57321325a5b5d8
Donald Ross <33313991+DJR-FP@users.noreply.github.com>
2019-12-19 21:37:30 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,3 @@
-
-Skip to content
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@DJR-FP 
-1
-00DJR-FP/webtest-RED
- Code Issues 0 Pull requests 0 Actions Projects 0 Wiki Security Insights Settings
-webtest-RED/index.php
-@DJR-FP DJR-FP Update index.php
-870b251 38 minutes ago
-105 lines (75 sloc)  2.06 KB
-  
-
 <?php
 echo ' 
 <!DOCTYPE html>
@@ -34,27 +15,34 @@ RED Test Site<br/>
 </body>
 </html>
 ';
+
 echo '<p style="color: white"/p>';
 //echo '<p style="font-size: 20px"/p>';
 //echo getenv('ENV_VAR');
+
 echo "Today Date is " . date("M,d,Y") . "<br>";
 echo "The Time is " . date("h:i:s") .  "<br>";
+
 echo "<br>";
 echo "<br>";
-echo "<br>";
-echo "<br>";
+
      // create a new cURL resource
       $ch = curl_init ();
+
       // set URL and other appropriate options
       curl_setopt ($ch, CURLOPT_URL, 'http://ipecho.net/plain');
       curl_setopt ($ch, CURLOPT_HEADER, 0);
       curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+
       // grab URL and pass it to the browser
+
       $ip = curl_exec ($ch);
       echo "Public IP is: $ip<br />";
       // close cURL resource, and free up system resources
       curl_close ($ch);
+
 echo "<br>";
+
 //whether ip is from share internet
 if (!empty($_SERVER['HTTP_CLIENT_IP']))   
   {
@@ -71,36 +59,38 @@ else
     $ip_address = $_SERVER['REMOTE_ADDR'];
   }
 echo "Client IP is $ip_address<br />";
+
 $SPORT = $_SERVER['REMOTE_PORT'];
 echo  "Client Source Port is $SPORT<br>";
+
 $rURI = $_SERVER['REQUEST_URI'];
 echo "Requested URI is $rURI<br>";
+
 echo "<br>";
+
 //echo file_get_contents("/tmp/host-hostname");
+
 $ServerName = file_get_contents("/tmp/host-hostname");
 echo "Server Name is $ServerName<br />"; 
+
 echo "<br>";
+
 $hostname = gethostname();
 echo "Server Container ID is $hostname<br />";
+
 $SIP = $_SERVER['SERVER_ADDR'];
 //$SIP = $_SERVER['SERVER_ADDR'];
 echo "Server Container IP is $SIP<br />";
+
 $SPORT = $_SERVER['SERVER_PORT'];
 echo "Server Container Port is $SPORT<br />";
+
+
+
 echo "<br>";
 $BROWSER = $_SERVER['HTTP_USER_AGENT'];
 echo "User Browser is $BROWSER<br/>";
 echo "<br>";
+
+
 ?>
-© 2019 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
